8c3fe99f6dd87fa626019c925169bcfc0b90f647
Пётр <dimok392@mail.ru>
2022-04-27 23:36:48 +0300

    example/example001/

---
+++ example/example001/Program.cs
@@ -0,0 +1,11 @@
+﻿// написать программу, которая принимает на вход число (А) и 
+// выдаёт сумму чисел от 1 до (А)
+Console.WriteLine("Введите число А:");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    Console.WriteLine(i);
+    sum +=i;//эквивалентно sum=sum+i
+    Console.WriteLine(sum);
+}
