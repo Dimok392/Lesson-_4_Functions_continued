8f8f49347833e4ea30352bb886a9bf72473ceaa4
Пётр <dimok392@mail.ru>
2022-04-28 00:45:05 +0300

    example/example002/

---
+++ example/example002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число А:");
+int A = int.Parse(Console.ReadLine());
+
+int SumOfnumbers(int N)    //объявление функции
+{
+    int sum = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        sum += i;//эквивалентно sum=sum+i
+        Console.WriteLine(i);
+    }
+    Console.WriteLine();
+    return sum;
+}
+
+  Console.WriteLine(SumOfnumbers(A));
